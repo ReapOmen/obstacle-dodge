95f5515939560834c0d3f20708985e70133a58c3
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-08-28 02:42:36 +0100

    Change the implemention of the constructor.

--- Classes/Paddle.cpp
+++ Classes/Paddle.cpp
@@ -2,10 +2,26 @@
 
 USING_NS_CC;
 
-Paddle::Paddle(Size windowSize)
+Paddle::Paddle(Size windowSize, int type)
 : AbstractSprite::AbstractSprite(windowSize, "paddle.png")
 {
-    setPosition(Vec2(windowSize.width / 2, windowSize.height / 2));
+    if(type == MIDDLE)
+    {
+        setPosition(Vec2(windowSize.width / 2,
+                         windowSize.height / 2));
+    }
+    else
+    if(type == LEFT)
+    {
+        setPosition(Vec2(windowSize.width/2 - getHalvedWidth()*3,
+                         windowSize.height / 2));
+    }
+    else
+    if(type == RIGHT)
+    {
+        setPosition(Vec2(windowSize.width/2 + getHalvedWidth()*3,
+                         windowSize.height / 2));
+    }
 }
 
 Paddle::~Paddle()
