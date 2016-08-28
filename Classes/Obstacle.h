8ccfdf6961b812840763d616313393f20c3942a9
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-08-28 02:43:47 +0100

    Initialise the Obstacle object.

---
+++ Classes/Obstacle.h
@@ -0,0 +1,24 @@
+#ifndef __OBSTACLE_H__
+#define __OBSTACLE_H__
+
+#include "cocos2d.h"
+#include "Paddle.h"
+
+class Obstacle
+{
+private:
+    std::vector<Paddle*> paddles;
+
+public:
+    static const int NONE = 0, LEFT = 1, MIDDLE = 2, RIGHT = 3, \
+                     NO_LEFT = 4, NO_MIDDLE = 5, NO_RIGHT = 6;
+
+    Obstacle(cocos2d::Size windowSize, int type);
+
+    virtual ~Obstacle();
+
+    std::vector<Paddle*> getPaddles() const;
+};
+
+
+#endif // __OBSTACLE_H__
