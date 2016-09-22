88749239ed57ed18b51fe7f7f9c069ec633a2875
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-09-22 16:12:47 +0100

    Add update method, obstacle height and Y position.

--- Classes/Obstacle.h
+++ Classes/Obstacle.h
@@ -9,6 +9,8 @@ class Obstacle
 private:
     std::vector<Paddle*> paddles;
 
+    float y, height, halvedHeight;
+
 public:
     static const int NONE = 0, LEFT = 1, MIDDLE = 2, RIGHT = 3, \
                      NO_LEFT = 4, NO_MIDDLE = 5, NO_RIGHT = 6;
@@ -18,7 +20,14 @@ public:
     virtual ~Obstacle();
 
     std::vector<Paddle*> getPaddles() const;
-};
 
+    virtual void update();
+
+    float getY() const;
+
+    float getHeight() const;
+
+    float getHalvedHeight() const;
+};
 
 #endif // __OBSTACLE_H__
