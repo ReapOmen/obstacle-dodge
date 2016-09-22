c05ebdda277f360b5944dbb36517b5f18fa50f7e
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-09-22 16:11:07 +0100

    Change starting point of paddles.

--- Classes/Paddle.cpp
+++ Classes/Paddle.cpp
@@ -5,26 +5,34 @@ USING_NS_CC;
 Paddle::Paddle(Size windowSize, int type)
 : AbstractSprite::AbstractSprite(windowSize, "paddle.png")
 {
+    float offscreen_y = windowSize.height + getHalvedHeight();
+
     if(type == MIDDLE)
     {
         setPosition(Vec2(windowSize.width / 2,
-                         windowSize.height / 2));
+                         offscreen_y));
     }
     else
     if(type == LEFT)
     {
         setPosition(Vec2(windowSize.width/2 - getHalvedWidth()*3,
-                         windowSize.height / 2));
+                         offscreen_y));
     }
     else
     if(type == RIGHT)
     {
         setPosition(Vec2(windowSize.width/2 + getHalvedWidth()*3,
-                         windowSize.height / 2));
+                         offscreen_y));
     }
 }
 
 Paddle::~Paddle()
 {
 
+}
+
+void Paddle::update()
+{
+    Vec2 pos = getPosition();
+    setPosition(Vec2(pos.x, pos.y - 10));
 }
