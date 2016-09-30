828e960c6f88ab479f4d6b1fa96d4abed83b29fb
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-09-30 16:54:27 +0100

    Add scale and speed members.

--- Classes/Globals.cpp
+++ Classes/Globals.cpp
@@ -7,6 +7,10 @@ Size Globals::screenSize = Director::getInstance()->getVisibleSize();
 float Globals::paddleY = 0.0f;
 float Globals::paddleHeight = 0.0f;
 float Globals::paddleHalvedHeight = 0.0f;
+float Globals::scaleObjects = 0.0f;
+float Globals::scaleButtons = 0.0f;
+float Globals::ballSpeed = 0.0f;
+float Globals::paddleSpeed = 0.0f;
 
 void Globals::init()
 {
@@ -15,4 +19,7 @@ void Globals::init()
     paddleHeight = paddle->getHeight();
     paddleHalvedHeight = paddle->getHalvedHeight();
     paddleY = paddle->getY();
+    scaleObjects = (0.2 * screenSize.width) / 1024;
+    ballSpeed = (10 * screenSize.width) / 1024;
+    paddleSpeed = (10 * screenSize.height) / 768;
 }
--- Classes/Globals.h
+++ Classes/Globals.h
@@ -9,6 +9,8 @@ public:
     static void init();
     static cocos2d::Size screenSize;
     static float paddleHeight, paddleHalvedHeight, paddleY;
+    static float scaleObjects, scaleButtons;
+    static float ballSpeed, paddleSpeed;
 };
 
 #endif // __GLOBALS_H__
